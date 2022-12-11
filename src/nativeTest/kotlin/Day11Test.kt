9a7207f978ed8638ba51d693ccce945dc7c66b66
Ruben Gees <rubengees7@gmail.com>
2022-12-12 00:44:11 +0100

    Add day 11

---
+++ src/nativeTest/kotlin/Day11Test.kt
@@ -0,0 +1,55 @@
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day11Test {
+    private val example = """
+        Monkey 0:
+          Starting items: 79, 98
+          Operation: new = old * 19
+          Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+        
+        Monkey 1:
+          Starting items: 54, 65, 75, 74
+          Operation: new = old + 6
+          Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+        
+        Monkey 2:
+          Starting items: 79, 60, 97
+          Operation: new = old * old
+          Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+        
+        Monkey 3:
+          Starting items: 74
+          Operation: new = old + 3
+          Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
+    """.trimIndent()
+
+    @Test
+    fun part1Example() {
+        assertEquals("10605", day11Part1(example))
+    }
+
+    @Test
+    fun part1Input() {
+        assertEquals("120056", day11Part1(readInputFile(11)))
+    }
+
+    @Test
+    fun part2Example() {
+        assertEquals("2713310158", day11Part2(example))
+    }
+
+    @Test
+    fun part2Input() {
+        assertEquals("21816744824", day11Part2(readInputFile(11)))
+    }
+}
